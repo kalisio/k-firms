6dcb2a952f0c0602840cd6d47460f38eb1f1a882
Luc Claustres <luc.claustres@orange.fr>
2022-07-19 14:38:31 +0200

    Initialized repository

---
+++ README.md
@@ -0,0 +1,40 @@
+# k-firms
+
+[![Latest Release](https://img.shields.io/github/v/tag/kalisio/k-firms?sort=semver&label=latest)](https://github.com/kalisio/k-firms/releases)
+[![Build Status](https://app.travis-ci.com/kalisio/k-firms.svg?branch=master)](https://app.travis-ci.com/kalisio/k-firms)
+
+A [Krawler](https://kalisio.github.io/krawler/) based service to download data from the [Fire Information for Resource Management System (FIRMS)](https://wifire-data.sdsc.edu/dataset/viirs-i-band-375-m-active-fire-data).
+
+## Description
+
+The **k-firms** job allows to scrape data from the FIRMS using the following url: [https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/csv/SUOMI_VIIRS_C2_Global_7d.csv](https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/csv/SUOMI_VIIRS_C2_Global_7d.csv)`. The downloaded data are stored within a [MongoDB](https://www.mongodb.com/) database:
+* the `firms` collection stores the Thermal Hotspots and Fire Activity data 
+
+All records are stored in [GeoJson](https://fr.wikipedia.org/wiki/GeoJSON) format.
+
+The job is executed according a specific cron expression. By default, every hour.
+
+## Configuration
+
+| Variable | Description |
+|--- | --- |
+| `DB_URL` | The mongoDB database URL. The default value is `mongodb://127.0.0.1:27017/kano` |
+| `DEBUG` | Enables debug output. Set it to `krawler*` to enable full output. By default it is undefined. |
+
+## Deployment
+
+We personally use [Kargo](https://kalisio.github.io/kargo/) to deploy the service.
+
+## Contributing
+
+Please refer to [contribution section](./CONTRIBUTING.md) for more details.
+
+## Authors
+
+This project is sponsored by 
+
+![Kalisio](https://s3.eu-central-1.amazonaws.com/kalisioscope/kalisio/kalisio-logo-black-256x84.png)
+
+## License
+
+This project is licensed under the MIT License - see the [license file](./LICENSE) for details
---
+++ jobfile.js
@@ -0,0 +1,108 @@
+import _ from 'lodash'
+import moment from 'moment'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/kano'
+
+export default {
+  id: 'firms',
+  store: 'memory',
+  taskTemplate: {
+  },
+  tasks: [{
+    id: 's-npp',
+    type: 'http',
+    options: {
+      url: 'https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/csv/SUOMI_VIIRS_C2_Global_7d.csv'
+    }
+  }],
+  hooks: {
+      tasks: {
+        before: {
+          readMostRecent: {
+            hook: 'readMongoCollection',
+            collection: 'firms',
+            dataPath: 'data.mostRecent',
+            query: {},
+            sort: { time: -1 },
+            limit: 1
+          } 
+        },
+        after: {
+            readCSV: {
+              headers: true,
+              transform: {
+                unitMapping: {
+                  latitude: { asNumber: true },
+                  longitude: { asNumber: true },
+                  bright_ti4: { asNumber: true },
+                  scan: { asNumber: true },
+                  track: { asNumber: true },
+                  frp: { asNumber: true },
+                  bright_ti5: { asNumber: true }
+                }
+              }
+            },
+            convertToGeoJson: {},
+            apply: {
+              function: (item) => {
+                // Ingest most recent only
+                const mostRecent = (_.isEmpty(item.mostRecent) ? null : moment.utc(item.mostRecent[0].time))
+                let features = []
+                _.forEach(item.data.features, (feature) => {
+                  const time = moment.utc(`${feature.properties.acq_date} ${feature.properties.acq_time}`, 'YYYY-MM-DD hhmm')
+                  // When collection is empty fill it in
+                  if (!mostRecent || time.isAfter(mostRecent)) {
+                    _.set(feature, 'time', time.toDate())
+                    features.push(feature)
+                  }
+                })
+                console.log(`Found ${features.length} new thermal hotspots` + (mostRecent ? ` after ${mostRecent}` : ''))
+                item.data.features = features
+              }
+            },/* For debug purpose
+            writeJson: {
+              store: 'fs'
+            },*/
+            writeMongoCollection: {
+              collection: 'firms',
+              chunkSize: 256
+            },
+            clearData: {}
+        },
+      },
+      jobs: {
+        before: {
+          createStore: [{
+            id: 'memory'
+          }, {
+            id: 'fs',
+            options: {
+              path: path.join(__dirname)
+            }
+          }],
+          connectMongo: {
+            url: dbUrl,
+            clientPath: 'taskTemplate.client'
+          },
+          createMongoCollection: {
+            clientPath: 'taskTemplate.client',
+            collection: 'firms',
+            indices: [
+              [{ time: -1 }, { background: true }]
+            ]
+          }
+        },
+        after: {
+          clearOutputs: {},
+          removeStores: ['memory', 'fs']
+        },
+        error: {
+          clearOutputs: {},
+          removeStores: ['memory', 'fs']
+        }
+      }
+  }
+}
