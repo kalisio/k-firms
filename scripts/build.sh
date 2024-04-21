22b28dc4e3b1656e879aa2155e5e2905039bac18
cnouguier <ch.nouguier@gmail.com>
2024-04-21 22:22:23 +0200

    wip: Refactor CI to use GitHub actions and generic bash scripts #1

---
+++ scripts/build.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# set -x
+
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_DIR=$(dirname "$THIS_FILE")
+ROOT_DIR=$(dirname "$THIS_DIR")
+WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
+
+. "$THIS_DIR/kash/kash.sh"
+
+## Parse options
+##
+
+PUBLISH=false
+CI_STEP_NAME="Build"
+while getopts "pr:" option; do
+    case $option in
+        p) # publish
+            PUBLISH=true
+            ;;
+        r) # report outcome to slack
+            CI_STEP_NAME=$OPTARG
+            load_env_files "$WORKSPACE_DIR/development/common/SLACK_WEBHOOK_JOBS.enc.env"
+            trap 'slack_ci_report "$ROOT_DIR" "$CI_STEP_NAME" "$?" "$SLACK_WEBHOOK_JOBS"' EXIT
+            ;;
+        *)
+            ;;
+    esac
+done
+
+## Init workspace
+##
+
+load_env_files "$WORKSPACE_DIR/development/common/kalisio_dockerhub.enc.env"
+load_value_files "$WORKSPACE_DIR/development/common/KALISIO_DOCKERHUB_PASSWORD.enc.value"
+
+## Build container
+##
+
+build_job \
+    "$ROOT_DIR" \
+    "kalisio" \
+    "" \
+    "$KALISIO_DOCKERHUB_URL" \
+    "$KALISIO_DOCKERHUB_USERNAME" \
+    "$KALISIO_DOCKERHUB_PASSWORD" \
+    "$PUBLISH"
